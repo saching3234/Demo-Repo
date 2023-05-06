75d762740991dec0666d0b919bb5616dc1c7c9b4
Sachin <saching3234@gmail.com>
2023-05-06 19:18:09 +0530

    added the multiply.java and diff.java file

--- Diff.java
+++ Diff.java
@@ -4,10 +4,8 @@ import java.util.List;
 public class Diff {
     
     public static void main(String[] args) {
-        List<String> list=new ArrayList<>();
-
-        list.add("Sachin");
-        list.add("ketan");
-        list.stream().forEach(e->System.out.println(e));
+     int a=10;
+     int b=5;
+     System.out.println("The diffence of two no is :"+(a-b));
     }
 }
---
+++ Multiply.java
@@ -0,0 +1,8 @@
+public class Multiply {
+
+    public static void main(String[] args) {
+        int a=10,b=20;
+        System.out.println("The multiplication of two  nos are: "+a*b);
+    }
+    
+}
