9ef56b637e3dd4905018c3dee08e0816469ac4b3
Sachin <saching3234@gmail.com>
2023-05-06 19:00:43 +0530

    Hello.java modified

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 class Hello{
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+    int a=10;
+    int b=20;
+
+     System.out.println("the sum of two nos is: "+(a+b));
     }
 }
